ddc0c0bcf6feb44e1d17cda92ac1473dfa6e95ab
Dxy <denight@qq.com>
2018-09-12 11:15:17 +0800

    init

---
+++ main.js
@@ -0,0 +1,14 @@
+const electron = require('electron')
+
+const {app, BrowserWindow} = electron
+
+function createWindow (){
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
