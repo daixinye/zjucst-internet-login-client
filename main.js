857f0fcf4a883eebd22d2ad087bfbacbbee454cc
Dxy <denight@qq.com>
2018-09-12 11:52:49 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 const electron = require('electron')
 
-const {app, BrowserWindow} = electron
+const { app, BrowserWindow } = electron
 
-function createWindow (){
-    let win = new BrowserWindow({
-        width: 800,
-        height: 600
-    })
+let win
 
-    win.loadFile('index.html')
+function createWindow () {
+  // 创建窗口
+  win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  // 加载index.html
+  win.loadFile('index.html')
+
+  win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
 }
 
 app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // macOS上没有任何窗口打开时要新开一个window
+  if (win === null) {
+    createWindow()
+  }
+})
