0783d8925e2e53292815aa9cd26346442179fd78
Dxy <denight@qq.com>
2018-11-09 23:05:35 +0800

    update: delete Menu class

--- main.js
+++ main.js
@@ -1,156 +1,135 @@
 const { app, Menu, Tray, Notification, nativeImage } = require('electron')
 const _path = require('path')
 
-const setting = require('./src/setting')
 const Client = require('./src/client')
-
 const Store = require('electron-store')
+
+const setting = require('./setting')
 const store = new Store()
 
-const appIcon = nativeImage.createFromPath(
-  _path.join(__dirname, setting.relative_path.icon)
-)
+let client = null
+let tray = null
 
-// app
-class Menus {
-  constructor () {
-    this.tray = null
-    this.contextMenu = null
-    this.client = null
-  }
-  init (icon) {
-    this.tray = new Tray(icon)
-    this.tray.setToolTip('zjucst-internet-login')
+const requireUserInfo = function () {
+  let noti = new Notification({
+    title: '需要账号和密码',
+    subtitle: '悬停此处，输入账号和密码，用英文冒号（:）隔开',
+    hasReply: true
+  })
 
-    let username = store.get('username')
-    let password = store.get('password')
-
+  noti.on('reply', (e, text) => {
+    text = text.split(':')
+    let username = text[0] || ''
+    let password = text[1] || ''
     if (username && password) {
-      this.client = new Client(username, password)
+      store.set('username', text[0] || '')
+      store.set('password', text[1] || '')
+      client = new Client(username, password, setting.url)
     } else {
-      this.requireUsernameAndPassword()
+      noti.show()
     }
-  }
-  setContextMenu (contextMenu) {
-    this.contextMenu = contextMenu
-    this.tray.setContextMenu(contextMenu)
-  }
-  requireUsernameAndPassword () {
-    let noti = new Notification({
-      title: '需要账号和密码',
-      subtitle: '悬停此处，输入账号和密码，用英文冒号（:）隔开',
-      hasReply: true
-    })
-
-    noti.on('reply', (e, text) => {
-      text = text.split(':')
-      let username = text[0] || ''
-      let password = text[1] || ''
-      if (username && password) {
-        store.set('username', text[0] || '')
-        store.set('password', text[1] || '')
-        this.client = new Client(username, password)
-      } else {
-        noti.show()
-      }
-    })
-
-    noti.show()
-  }
-  check () {
-    if (!this.client) {
-      this.requireUsernameAndPassword()
-      return false
-    }
-    return true
-  }
-  clear () {
-    let username = store.get('username')
-
-    store.delete('username')
-    store.delete('password')
-    this.client = null
-
-    new Notification({
-      title: '清除登录信息成功',
-      subtitle: username
-    }).show()
-  }
-
-  login () {
-    if (this.check()) {
-      let loginResult = menus.client.login()
-      loginResult
-        .then(res => {
-          new Notification({
-            title: '登录成功',
-            subtitle: res
-          }).show()
-        })
-        .catch(res => {
-          new Notification({
-            title: '登录失败',
-            subtitle: res
-          }).show()
-        })
-    }
-  }
-
-  logout () {
-    if (this.check()) {
-      let logoutResult = menus.client.logout()
-      logoutResult
-        .then(res => {
-          new Notification({
-            title: '下线成功',
-            subtitle: res
-          }).show()
-        })
-        .catch(res => {
-          new Notification({
-            title: '下线失败',
-            subtitle: res
-          }).show()
-        })
-    }
-  }
+  })
+  noti.show()
 }
-
-let menus = new Menus()
 
 app.on('will-finish-launching', () => {
   app.dock.hide()
 })
 
 app.on('ready', () => {
-  menus.init(appIcon)
+  tray = new Tray(
+    nativeImage.createFromPath(
+      _path.join(__dirname, setting.relative_path.icon)
+    )
+  )
+  tray.setToolTip('zjucst-internet-login-client')
+
+  let username = store.get('username')
+  let password = store.get('password')
+
+  if (username && password) {
+    client = new Client(username, password, setting.url)
+  } else {
+    requireUserInfo()
+  }
+
   const contextMenu = Menu.buildFromTemplate([
-    { label: '状态', type: 'normal', enabled: false },
-    {
-      type: 'separator'
-    },
     {
       label: '上线',
       type: 'normal',
-      click: item => {
-        menus.login()
+      click: () => {
+        if (!client) {
+          return requireUserInfo()
+        }
+        client
+          .login()
+          .then(res => {
+            new Notification({
+              title: '登录成功',
+              subtitle: res
+            }).show()
+          })
+          .catch(res => {
+            new Notification({
+              title: '登录失败',
+              subtitle: res
+            }).show()
+
+            if (res === 'online_num_error') {
+              // todo: 添加 强制下线自动登录 逻辑
+            }
+          })
       }
     },
     {
       label: '下线',
       type: 'normal',
-      checked: true,
-      click: item => {
-        menus.logout()
+      click: () => {
+        if (!client) {
+          return requireUserInfo()
+        }
+        client
+          .logout()
+          .then(res => {
+            new Notification({
+              title: '下线成功',
+              subtitle: res
+            }).show()
+          })
+          .catch(res => {
+            new Notification({
+              title: '下线失败',
+              subtitle: res
+            }).show()
+          })
+      }
+    },
+    {
+      label: '强制下线',
+      type: 'normal',
+      click: () => {
+        if (!client) {
+          return requireUserInfo()
+        }
+
+        client
+          .forceLogout()
+          .then(res => {
+            new Notification({
+              title: '强制下线成功',
+              subtitle: res
+            }).show()
+          })
+          .catch(res => {
+            new Notification({
+              title: '强制下线失败',
+              subtitle: res
+            }).show()
+          })
       }
     },
     {
       type: 'separator'
-    },
-    {
-      label: '设置',
-      type: 'normal',
-      enabled: false
     },
     {
       type: 'separator'
@@ -159,7 +138,16 @@ app.on('ready', () => {
       label: '清除登录信息',
       type: 'normal',
       click: () => {
-        menus.clear()
+        let username = store.get('username')
+
+        store.delete('username')
+        store.delete('password')
+        this.client = null
+
+        new Notification({
+          title: '清除登录信息成功',
+          subtitle: username
+        }).show()
       }
     },
     {
@@ -170,5 +158,5 @@ app.on('ready', () => {
       }
     }
   ])
-  menus.setContextMenu(contextMenu)
+  tray.setContextMenu(contextMenu)
 })
