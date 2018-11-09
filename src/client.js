2926e726689deee58301b59a78d1e6a5abd8f679
Dxy <denight@qq.com>
2018-11-09 23:04:21 +0800

    update: use promise & throw some errors

--- src/client.js
+++ src/client.js
@@ -1,79 +1,129 @@
-const _md5 = require('./md5')
-const setting = require('./setting')
-const { post } = require('./post')
+const _md5 = require('../lib/md5')
+const { post } = require('../lib/post')
 
 class Client {
-  constructor (username, password) {
-    this.username = username
-    this.password = password
-    this.uid = ''
+  constructor (username = null, password = null, urls = null) {
+    if (!username) {
+      throw Error('username 不能为空')
+    }
+    if (!password) {
+      throw Error('password 不能为空')
+    }
+    if (!urls) {
+      throw Error('urls 不能为空')
+    } else {
+      if (!urls.login) {
+        throw Error('urls 中缺少 login url')
+      }
+      if (!urls.logout) {
+        throw Error('urls 中缺少 logout url')
+      }
+      if (!urls.forceLogout) {
+        throw Error('urls 中缺少 forceLogout url')
+      }
+    }
+
+    this._username = username
+    this._password = password
+    this._urls = urls
+    this._uid = ''
   }
+
   login () {
+    let username = this._username
+    let password = this._password
+    let url = this._urls.login
+
     let postData = {
-      username: this.username,
-      password: _md5.hex_md5(this.password).substr(8, 16),
+      username: username,
+      password: _md5.hex_md5(password).substr(8, 16),
       drop: 0,
       type: 1,
       n: 100
     }
 
-    let url = setting.url.login
-
     return new Promise((resolve, reject) => {
-      post(url, postData, res => {
-        let pattern = /^[\d]+$/
-        if (pattern.test(res)) {
-          this.uid = res
-          resolve(res)
-        } else {
-          switch (res) {
-            case 'online_num_error':
-              // 正常退出失败->强制登出
-              this.forceLogout()
-              // 每隔5秒尝试登陆直到登陆成功
-              setTimeout(() => {
-                this.login()
-              }, 5 * 1000)
-              break
-            default:
-              reject(res)
+      post(url, postData)
+        .then(res => {
+          let pattern = /^[\d]+$/
+          if (pattern.test(res)) {
+            this._uid = res
+            resolve(res)
+          } else {
+            reject(res)
           }
-        }
-      })
+        })
+        .catch(e => {
+          console.error(e)
+          reject(e)
+        })
     })
   }
 
   logout () {
+    let uid = this._uid
+    let url = this._urls.logout
+
     let postData = {
-      uid: this.uid
+      uid
     }
-    let url = setting.url.logout
 
     return new Promise((resolve, reject) => {
-      post(url, postData, res => {
-        if (res === 'logout_ok') {
-          resolve(res)
-        } else {
-          reject(res)
-        }
-      })
+      post(url, postData)
+        .then(res => {
+          if (res === 'logout_ok') {
+            resolve(res)
+          } else {
+            reject(res)
+          }
+        })
+        .catch(e => {
+          console.error(e)
+          reject(e)
+        })
     })
   }
 
   forceLogout () {
+    let username = this._username
+    let password = this._password
+    let url = this._urls.forceLogout
+
     let postData = {
-      username: this.username,
-      password: this.password,
+      username: username,
+      password: password,
       drop: 0,
       type: 1,
       n: 1
     }
-    let url = setting.url.forceLogout
 
-    post(url, postData, res => {
-      console.log(res)
+    return new Promise((resolve, reject) => {
+      post(url, postData)
+        .then(res => {
+          if (res === 'logout_ok') {
+            resolve(res)
+          } else {
+            reject(res)
+          }
+        })
+        .catch(e => {
+          console.error(e)
+          reject(e)
+        })
     })
   }
 }
 
 module.exports = Client
+
+if (require.main === module) {
+  const setting = require('../setting')
+  let username = process.argv[2]
+  let password = process.argv[3]
+  let urls = setting.url
+
+  const client = new Client(username, password, urls)
+  client.forceLogout().then(res => {
+    console.log(res)
+  })
+}
